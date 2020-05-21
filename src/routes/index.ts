049e90c669b077307c2b8296d3e43b45621b7e8c
Torres <sergio.uft@gmail.com>
2020-05-21 17:04:11 -0300

    chore: add ensured authenticate middleware to appointments routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,11 @@ import { Router } from 'express';
 import appointmentsRouter from './appointments.routes';
 import userRouter from './users.routes';
 import sessionRouter from './sessions.routes';
+import ensuredAuthenticated from '../middlewares/ensuredAuthenticated';
 
 const routes = Router();
 
-routes.use('/appointments', appointmentsRouter);
+routes.use('/appointments', ensuredAuthenticated, appointmentsRouter);
 routes.use('/users', userRouter);
 routes.use('/sessions', sessionRouter);
 

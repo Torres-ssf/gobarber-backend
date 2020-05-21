d382db78f18722934a732adcc294c3d8dfba8f8d
Torres <sergio.uft@gmail.com>
2020-05-21 18:18:24 -0300

    feat: implement globlal error handling middleware in server file

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import 'reflect-metadata';
 
-import express, { json } from 'express';
+import express, { json, Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import routes from './routes';
 import uploadConfig from './config/upload';
 
 import './database';
+import AppError from './errors/AppError';
 
 const app = express();
 
@@ -13,6 +15,20 @@ app.use(json());
 app.use('/files', express.static(uploadConfig.directory));
 
 app.use(routes);
+
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return res
+      .status(err.statusCode)
+      .json({ status: 'error', message: err.message });
+  }
+
+  console.error(err);
+
+  return res
+    .status(500)
+    .json({ status: 'error', message: 'Internal server error' });
+});
 
 app.listen(3333, () => {
   console.log('Server started on port 3333!');

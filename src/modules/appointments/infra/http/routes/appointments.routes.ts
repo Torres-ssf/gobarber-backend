3b4823c2a2183f36c8ad6075fb4f4a0017413865
Torres <sergio.uft@gmail.com>
2020-05-22 18:23:24 -0300

    refactor: update appointments route to use AppointmentsController

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,40 +1,15 @@
 import { Router } from 'express';
-import { startOfHour, parseISO } from 'date-fns';
-
-import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
-import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 import ensuredAuthenticated from '@modules/users/infra/http/middlewares/ensuredAuthenticated';
+import AppointmentsControllers from '@modules/appointments/infra/http/controllers/AppointmentsController';
 
 const appointmentsRouter = Router();
+const appointmentsController = new AppointmentsControllers();
 
 appointmentsRouter.use(ensuredAuthenticated);
 
-appointmentsRouter.get('/', async (req, res) => {
-  const appointmentsRepository = new AppointmentsRepository();
+appointmentsRouter.get('/', appointmentsController.index);
 
-  const appointments = await appointmentsRepository.show();
-
-  return res.json(appointments);
-});
-
-appointmentsRouter.post('/', async (req, res) => {
-  const { provider_id, date } = req.body;
-
-  const parsedDate = startOfHour(parseISO(date));
-
-  const appointmentsRepository = new AppointmentsRepository();
-
-  const createAppointmentService = new CreateAppointmentService(
-    appointmentsRepository,
-  );
-
-  const appointment = await createAppointmentService.execute({
-    provider_id,
-    date: parsedDate,
-  });
-
-  return res.json({ appointment });
-});
+appointmentsRouter.post('/', appointmentsController.create);
 
 export default appointmentsRouter;

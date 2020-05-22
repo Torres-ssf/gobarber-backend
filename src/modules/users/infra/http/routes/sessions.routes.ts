c6240dfb5ce6fc4c02e0baffc4a4f304d386b559
Torres <sergio.uft@gmail.com>
2020-05-22 18:20:51 -0300

    refactor: update sessions route to use UserAvatarController

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,25 +1,10 @@
 import { Router } from 'express';
 
-import UserRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
-import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
+import SessionsController from '@modules/users/infra/http/controllers/SessionsController';
 
 const sessionRoutes = Router();
+const sessionsController = new SessionsController();
 
-sessionRoutes.post('/', async (req, res) => {
-  const { email, password } = req.body;
-
-  const userRepository = new UserRepository();
-
-  const authenticateService = new AuthenticateUserService(userRepository);
-
-  const { user, token } = await authenticateService.execute({
-    email,
-    password,
-  });
-
-  delete user.password;
-
-  return res.status(200).json({ user, token });
-});
+sessionRoutes.post('/', sessionsController.create);
 
 export default sessionRoutes;

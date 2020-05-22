99ff6dcf911ae33eb19b8cf117f99f3b0296f178
Torres <sergio.uft@gmail.com>
2020-05-22 18:14:08 -0300

    refactor: update user routes to use UserController

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -2,45 +2,23 @@ import { Router } from 'express';
 import multer from 'multer';
 import uploadConfig from '@config/upload';
 
-import UserRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
-import CreateUserService from '@modules/users/services/CreateUserService';
-import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
+import UsersController from '@modules/users/infra/http/controllers/UsersController';
+import UserAvatarController from '@modules/users/infra/http/controllers/UserAvatarController';
 
 import ensuredAuthenticated from '@modules/users/infra/http/middlewares/ensuredAuthenticated';
 
 const userRoutes = Router();
+const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 const upload = multer(uploadConfig);
 
-userRoutes.post('/', async (req, res) => {
-  const { name, email, password } = req.body;
-
-  const userRepository = new UserRepository();
-
-  const createUserService = new CreateUserService(userRepository);
-
-  const user = await createUserService.execute({ name, email, password });
-
-  return res.status(200).json(user);
-});
+userRoutes.post('/', usersController.create);
 
 userRoutes.patch(
   '/avatar',
   ensuredAuthenticated,
   upload.single('avatar'),
-  async (req, res) => {
-    const userRepository = new UserRepository();
-
-    const updateUserAvatarService = new UpdateUserAvatarService(userRepository);
-
-    const user = await updateUserAvatarService.execute({
-      user_id: req.user.id,
-      avatarFilename: req.file.filename,
-    });
-
-    delete user.password;
-
-    return res.status(200).json(user);
-  },
+  userAvatarController.update,
 );
 
 export default userRoutes;

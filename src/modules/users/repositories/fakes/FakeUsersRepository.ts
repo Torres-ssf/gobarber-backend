6d3276e999d699fcea90d98724eb39a0815a9caa
Torres <sergio.uft@gmail.com>
2020-05-22 23:05:31 -0300

    chore: create FakeUsersRepository

---
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -0,0 +1,37 @@
+import { uuid } from 'uuidv4';
+import User from '@modules/users/infra/typeorm/entities/User';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IUserCreateDTO from '@modules/users/dtos/ICreateUserDTO';
+
+class UsersRepository implements IUsersRepository {
+  private users: User[] = [];
+
+  async create(userData: IUserCreateDTO): Promise<User> {
+    const user = new User();
+
+    Object.assign(user, { id: uuid() }, userData);
+
+    this.users.push(user);
+
+    return user;
+  }
+
+  async findById(userId: string): Promise<User | undefined> {
+    return this.users.find(user => user.id === userId);
+  }
+
+  async findByEmail(email: string): Promise<User | undefined> {
+    return this.users.find(user => user.email === email);
+  }
+
+  async save(user: User): Promise<User> {
+    const userIndex = this.users.findIndex(findUser => findUser.id === user.id);
+
+    this.users[userIndex] = user;
+
+    return user;
+  }
+}
+
+export default UsersRepository;

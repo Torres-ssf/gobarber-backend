712acacb5c5e651870de01c4297c53e41323a9fb
Torres <sergio.uft@gmail.com>
2020-05-23 16:19:38 -0300

    refactor: made UserAvatarController inject a StorageProvider object into UpdateAvatarService

--- src/modules/users/infra/http/controllers/UserAvatarController.ts
+++ src/modules/users/infra/http/controllers/UserAvatarController.ts
@@ -2,12 +2,18 @@ import { Request, Response } from 'express';
 
 import UserRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
+import DiskStorageProvider from '@shared/providers/StorageProvider/implementations/DiskStorageProvider';
 
 export default class UserAvatarController {
   public async update(req: Request, res: Response): Promise<Response> {
     const userRepository = new UserRepository();
 
-    const updateUserAvatarService = new UpdateUserAvatarService(userRepository);
+    const diskStorageProvider = new DiskStorageProvider();
+
+    const updateUserAvatarService = new UpdateUserAvatarService(
+      userRepository,
+      diskStorageProvider,
+    );
 
     const user = await updateUserAvatarService.execute({
       user_id: req.user.id,

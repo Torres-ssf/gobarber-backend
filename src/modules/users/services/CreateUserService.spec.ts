67eadfe8cbed9dd244b2e1e3cc1ae652d0809f46
Torres <sergio.uft@gmail.com>
2020-05-22 23:20:44 -0300

    test: create user with taken email

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -1,5 +1,6 @@
 import CreateUserService from '@modules/users/services/CreateUserService';
 import FakeAppointmentRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import AppError from '@shared/errors/AppError';
 
 describe('CreateUser', () => {
   it('should create a user', async () => {
@@ -16,5 +17,25 @@ describe('CreateUser', () => {
     expect(user).toBeTruthy();
     expect(user).toHaveProperty('id');
     expect(user.name).toBe('John');
+  });
+
+  it('should not be to create a new user  with an email already registered', async () => {
+    const fakeUserRepository = new FakeAppointmentRepository();
+
+    const createUserService = new CreateUserService(fakeUserRepository);
+
+    await createUserService.execute({
+      name: 'John',
+      email: 'john@mail.com',
+      password: '123456',
+    });
+
+    expect(
+      createUserService.execute({
+        name: 'John Paul',
+        email: 'john@mail.com',
+        password: '654321',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });

08a7b667be2cddcb4f26dfa22a9ad0729ada8bb8
Torres <sergio.uft@gmail.com>
2020-05-22 22:49:55 -0300

    test: not create 2 appointments in the same time

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -16,4 +16,24 @@ describe('CreateAppointment', () => {
     expect(appointment).toHaveProperty('id');
     expect(appointment.provider_id).toBe('newProviderId');
   });
+
+  it('should not be able to create 2 appointments in the same time', async () => {
+    const fakeAppointment = new FakeAppointmentRepository();
+
+    const createAppointment = new CreateAppointment(fakeAppointment);
+
+    const appointmentDate = new Date();
+
+    await createAppointment.execute({
+      provider_id: 'newProviderId',
+      date: appointmentDate,
+    });
+
+    expect(
+      createAppointment.execute({
+        provider_id: 'newProviderId',
+        date: appointmentDate,
+      }),
+    ).rejects.toBeInstanceOf(Error);
+  });
 });

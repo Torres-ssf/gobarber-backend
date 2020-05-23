30be09bb2eeb69345cab2d0f27a104ab0cb6cde7
Torres <sergio.uft@gmail.com>
2020-05-23 11:11:15 -0300

    test: not authenticate uncreated user

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -2,6 +2,7 @@ import AuthenticateUserService from '@modules/users/services/AuthenticateUserSer
 import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHashProvider';
 import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
 import CreateUserService from '@modules/users/services/CreateUserService';
+import AppError from '@shared/errors/AppError';
 
 describe('AuthenticateUser', () => {
   it('should be able to authenticate created user', async () => {
@@ -30,5 +31,23 @@ describe('AuthenticateUser', () => {
     const res = await authenticateUserService.execute({ email, password });
 
     expect(res).toHaveProperty('token');
+  });
+
+  it('should not be able to authenticate an uncreated user', async () => {
+    const fakeUserRepository = new FakeUsersRepository();
+
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUserService = new AuthenticateUserService(
+      fakeUserRepository,
+      fakeHashProvider,
+    );
+
+    expect(
+      authenticateUserService.execute({
+        email: 'john@mail.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });

7f82681cbef749cc7d6fb926f3f19d29dd797c81
Torres <sergio.uft@gmail.com>
2020-05-23 18:42:04 -0300

    test: updateUserAvatarService should delete old avatar

--- src/modules/users/services/UpdateUserAvatarService.spec.ts
+++ src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -1,6 +1,7 @@
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 import FakeUserRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
 import FakeStorageProvider from '@shared/providers/StorageProvider/fakes/FakeStorageProvider';
+import AppError from '@shared/errors/AppError';
 
 describe('CreateUser', () => {
   it('should be able to update user avatar', async () => {
@@ -27,5 +28,54 @@ describe('CreateUser', () => {
     });
 
     expect(user.avatar).toBe('avatar.png');
+  });
+
+  it('should not be able to update avatar from a non logged user', async () => {
+    const fakeUserRepository = new FakeUserRepository();
+
+    const fakeStorageProvider = new FakeStorageProvider();
+
+    const updateUserAvatarService = new UpdateUserAvatarService(
+      fakeUserRepository,
+      fakeStorageProvider,
+    );
+
+    expect(
+      updateUserAvatarService.execute({
+        user_id: 'not-logged-user',
+        avatarFilename: 'avatar.png',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should delete old avatar when updating a new one', async () => {
+    const fakeUserRepository = new FakeUserRepository();
+
+    const fakeStorageProvider = new FakeStorageProvider();
+
+    const updateUserAvatarService = new UpdateUserAvatarService(
+      fakeUserRepository,
+      fakeStorageProvider,
+    );
+
+    const deleteFile = jest.spyOn(fakeStorageProvider, 'deleteFile');
+
+    const user = await fakeUserRepository.create({
+      name: 'Paul Test',
+      email: 'paul@email.com',
+      password: '123456',
+    });
+
+    await updateUserAvatarService.execute({
+      user_id: user.id,
+      avatarFilename: 'avatar.png',
+    });
+
+    await updateUserAvatarService.execute({
+      user_id: user.id,
+      avatarFilename: 'new-avatar.png',
+    });
+
+    expect(deleteFile).toBeCalledWith('avatar.png');
   });
 });

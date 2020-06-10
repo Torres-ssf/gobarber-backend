8dfbb7ae795e5f74bf53da9fe2914107cbc3fe3c
Torres <sergio.uft@gmail.com>
2020-06-10 15:47:52 -0300

    feat: add existent user verification

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,5 +1,6 @@
 import UsersRepository from '@modules/users/repositories/IUsersRepository';
 import MailRepository from '@shared/providers/MailProvider/models/IMailProvider';
+import AppError from '@shared/errors/AppError';
 
 interface IRequest {
   email: string;
@@ -19,6 +20,11 @@ class SendForgotPasswordEmailService {
   }
 
   public async execute({ email }: IRequest): Promise<void> {
+    const userExists = await this.usersRepository.findByEmail(email);
+
+    if (!userExists) {
+      throw new AppError('User does not exists');
+    }
     this.mailRepository.sendMail(email, 'change password request received');
   }
 }

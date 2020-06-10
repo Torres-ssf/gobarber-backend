2b0dac780ac3e98381b1c4fda8d92ca5b15e780c
Torres <sergio.uft@gmail.com>
2020-06-10 16:34:57 -0300

    feat: add password token generation

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,22 +1,27 @@
-import UsersRepository from '@modules/users/repositories/IUsersRepository';
-import MailRepository from '@shared/providers/MailProvider/models/IMailProvider';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IMailRepository from '@shared/providers/MailProvider/models/IMailProvider';
 import AppError from '@shared/errors/AppError';
+import IUsersTokenRepository from '../repositories/IUserTokensRepository';
 
 interface IRequest {
   email: string;
 }
 
 class SendForgotPasswordEmailService {
-  private usersRepository: UsersRepository;
+  private usersRepository: IUsersRepository;
 
-  private mailRepository: MailRepository;
+  private mailRepository: IMailRepository;
+
+  private tokenRepository: IUsersTokenRepository;
 
   constructor(
-    usersRepository: UsersRepository,
-    mailRepository: MailRepository,
+    usersRepository: IUsersRepository,
+    mailRepository: IMailRepository,
+    tokensRepository: IUsersTokenRepository,
   ) {
     this.usersRepository = usersRepository;
     this.mailRepository = mailRepository;
+    this.tokenRepository = tokensRepository;
   }
 
   public async execute({ email }: IRequest): Promise<void> {
@@ -25,6 +30,9 @@ class SendForgotPasswordEmailService {
     if (!userExists) {
       throw new AppError('User does not exists');
     }
+
+    await this.tokenRepository.generate(userExists.id);
+
     this.mailRepository.sendMail(email, 'change password request received');
   }
 }

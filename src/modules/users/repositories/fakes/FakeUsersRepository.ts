03938f786e9830400342afd6aba56b5d5b9d0f49
Torres <sergio.uft@gmail.com>
2020-06-10 16:16:21 -0300

    chore: create FakeUserTokensRepository

--- src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -4,7 +4,7 @@ import User from '@modules/users/infra/typeorm/entities/User';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import IUserCreateDTO from '@modules/users/dtos/ICreateUserDTO';
 
-class UsersRepository implements IUsersRepository {
+class FakeUsersRepository implements IUsersRepository {
   private users: User[] = [];
 
   async create(userData: IUserCreateDTO): Promise<User> {
@@ -34,4 +34,4 @@ class UsersRepository implements IUsersRepository {
   }
 }
 
-export default UsersRepository;
+export default FakeUsersRepository;

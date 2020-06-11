a812f2eece2e89ec7803fc9580d7f4ece2666d09
Torres <sergio.uft@gmail.com>
2020-06-11 11:01:00 -0300

    refactor: update controllers to conform with tsyringe dependency injection

--- src/modules/users/infra/http/controllers/SessionsController.ts
+++ src/modules/users/infra/http/controllers/SessionsController.ts
@@ -1,21 +1,13 @@
 import { Request, Response } from 'express';
+import { container } from 'tsyringe';
 
-import UserRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
-import BCryptHashProvider from '@modules/users/providers/HashProvider/implementations/BCryptHashProvider';
 
 export default class SessionsController {
   public async create(req: Request, res: Response): Promise<Response> {
     const { email, password } = req.body;
 
-    const userRepository = new UserRepository();
-
-    const bcryptHashProvider = new BCryptHashProvider();
-
-    const authenticateService = new AuthenticateUserService(
-      userRepository,
-      bcryptHashProvider,
-    );
+    const authenticateService = container.resolve(AuthenticateUserService);
 
     const { user, token } = await authenticateService.execute({
       email,
--- src/modules/users/infra/http/controllers/UserAvatarController.ts
+++ src/modules/users/infra/http/controllers/UserAvatarController.ts
@@ -1,19 +1,11 @@
 import { Request, Response } from 'express';
+import { container } from 'tsyringe';
 
-import UserRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
-import DiskStorageProvider from '@shared/providers/StorageProvider/implementations/DiskStorageProvider';
 
 export default class UserAvatarController {
   public async update(req: Request, res: Response): Promise<Response> {
-    const userRepository = new UserRepository();
-
-    const diskStorageProvider = new DiskStorageProvider();
-
-    const updateUserAvatarService = new UpdateUserAvatarService(
-      userRepository,
-      diskStorageProvider,
-    );
+    const updateUserAvatarService = container.resolve(UpdateUserAvatarService);
 
     const user = await updateUserAvatarService.execute({
       user_id: req.user.id,

733cfde018461600933975154bcb5931ab1c1693
Torres <sergio.uft@gmail.com>
2020-06-14 21:08:04 -0300

    feat: finish implement profile update functionality

--- src/modules/users/services/UpdateProfileService.ts
+++ src/modules/users/services/UpdateProfileService.ts
@@ -1,6 +1,8 @@
 import { injectable, inject } from 'tsyringe';
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUserRepository from '@modules/users/repositories/IUsersRepository';
+import IHashProvider from '@modules/users/providers/HashProvider/implementations/BCryptHashProvider';
+import AppError from '@shared/errors/AppError';
 
 interface IResquest {
   user_id: string;
@@ -14,19 +16,60 @@ interface IResquest {
 class UpdateProfileService {
   private usersRepository: IUserRepository;
 
+  private hashProvider: IHashProvider;
+
   constructor(
     @inject('UsersRepository')
     usersRepository: IUserRepository,
+    @inject('HashProvider')
+    hashProvider: IHashProvider,
   ) {
     this.usersRepository = usersRepository;
+    this.hashProvider = hashProvider;
   }
 
-  public async execute({ user_id, name, email }: IResquest): Promise<User> {
-    return this.usersRepository.create({
-      name,
-      email,
-      password: '123123',
-    });
+  public async execute({
+    user_id,
+    name,
+    email,
+    new_password,
+    old_password,
+  }: IResquest): Promise<User> {
+    const user = await this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('Unable to find user with given user id');
+    }
+
+    const userWithProvidedEmail = await this.usersRepository.findByEmail(email);
+
+    if (userWithProvidedEmail && user.id !== userWithProvidedEmail.id) {
+      throw new AppError('Email already in user');
+    }
+
+    user.name = name;
+    user.email = email;
+
+    if (new_password && !old_password) {
+      throw new AppError('The old password is required to set a new password');
+    }
+
+    if (new_password && old_password) {
+      const passwordMatch = await this.hashProvider.compare(
+        old_password,
+        user.password,
+      );
+
+      if (!passwordMatch) {
+        throw new AppError('Current password does not match');
+      }
+
+      user.password = await this.hashProvider.generateHash(new_password);
+    }
+
+    await this.usersRepository.save(user);
+
+    return user;
   }
 }
 

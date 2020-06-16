8ef8f47e29d4d924d1089e6470b08b844a37d03b
Torres <sergio.uft@gmail.com>
2020-06-16 16:53:11 -0300

    test: implement ListProviderDayAvailabilityService

--- src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -47,6 +47,67 @@ describe('ListProviderDayAvailabilityService', () => {
         { hour: randomNumber({ min: 8, max: 17 }), available: false },
         { hour: randomNumber({ min: 8, max: 17 }), available: false },
         { hour: randomNumber({ min: 8, max: 17 }), available: false },
+        { hour: randomNumber({ min: 8, max: 17 }), available: false },
+      ]),
+    );
+  });
+
+  it('should be able to invalidate past time', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 12, 11).getTime();
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'provider01',
+      date: new Date(2020, 4, 12, 13),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'provider01',
+      date: new Date(2020, 4, 12, 15),
+    });
+
+    const appointments = await listProviderDayAvailabilityService.execute({
+      provider_id: 'provider01',
+      day: 12,
+      month: 5,
+      year: 2020,
+    });
+
+    const pastAppointments = await listProviderDayAvailabilityService.execute({
+      provider_id: 'provider01',
+      day: 1,
+      month: 1,
+      year: 2020,
+    });
+
+    expect(appointments).toEqual(
+      expect.arrayContaining([
+        { hour: 8, available: false },
+        { hour: 9, available: false },
+        { hour: 10, available: false },
+        { hour: 11, available: false },
+        { hour: 12, available: true },
+        { hour: 13, available: false },
+        { hour: 14, available: true },
+        { hour: 15, available: false },
+        { hour: 16, available: true },
+        { hour: 17, available: true },
+      ]),
+    );
+
+    expect(pastAppointments).toEqual(
+      expect.arrayContaining([
+        { hour: 8, available: false },
+        { hour: 9, available: false },
+        { hour: 10, available: false },
+        { hour: 11, available: false },
+        { hour: 12, available: false },
+        { hour: 13, available: false },
+        { hour: 14, available: false },
+        { hour: 15, available: false },
+        { hour: 16, available: false },
+        { hour: 17, available: false },
       ]),
     );
   });

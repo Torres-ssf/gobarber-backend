b5a2d72267c422b85463ae81e760190248ed930a
Torres <sergio.uft@gmail.com>
2020-06-15 22:44:31 -0300

    feat: create IAppointmentsRepository

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,8 +1,12 @@
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
   show(): Promise<Appointment[]>;
   findByDate(date: Date): Promise<Appointment | undefined>;
+  findAllInMonthFromProviderDTO(
+    data: IFindAllInMonthFromProviderDTO,
+  ): Promise<Appointment[]>;
 }

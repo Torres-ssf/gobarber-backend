de9fa3f3dbaa72ba9763cb105220818838262365
Torres <sergio.uft@gmail.com>
2020-06-16 22:32:48 -0300

    feat: add new business rules to CreateAppointmentService

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,4 @@
-import { startOfHour } from 'date-fns';
+import { startOfHour, isBefore, getHours } from 'date-fns';
 import { inject, injectable } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
@@ -30,6 +30,22 @@ class CreteAppointmentService {
     date,
   }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
+
+    if (provider_id === user_id) {
+      throw new AppError("You can't create an appointment with yourself");
+    }
+
+    if (isBefore(appointmentDate, Date.now())) {
+      throw new AppError("Can't create an appointment on a past date");
+    }
+
+    const hour = getHours(appointmentDate);
+
+    if (hour < 8 || hour > 17) {
+      throw new AppError(
+        "Appointments can't be created before 8am and after 5pm",
+      );
+    }
 
     const findAppointment = await this.appointmentsRepository.findByDate(
       appointmentDate,

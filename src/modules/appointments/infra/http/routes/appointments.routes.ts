3f36df8112d68569c744911b4d950df07bb573d2
Torres <sergio.uft@gmail.com>
2020-06-21 12:30:02 -0300

    feat: use celebrate to validade appointments routes

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensuredAuthenticated from '@modules/users/infra/http/middlewares/ensuredAuthenticated';
 import AppointmentsController from '@modules/appointments/infra/http/controllers/AppointmentsController';
@@ -10,10 +11,17 @@ const listProviderAppointmentsController = new ListProviderAppointmentsControlle
 
 appointmentsRouter.use(ensuredAuthenticated);
 
-appointmentsRouter.get('/', appointmentsController.index);
+appointmentsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      provider_id: Joi.string().uuid().required(),
+      date: Joi.string().required(),
+    },
+  }),
+  appointmentsController.create,
+);
 
 appointmentsRouter.get('/me', listProviderAppointmentsController.index);
-
-appointmentsRouter.post('/', appointmentsController.create);
 
 export default appointmentsRouter;

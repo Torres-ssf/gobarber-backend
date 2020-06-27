54cf22ae970a48cf45e73da6f9ac8de457996ead
Torres <sergio.uft@gmail.com>
2020-06-27 14:03:07 -0300

    fix: receive date from query instead of body

--- src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -5,7 +5,7 @@ import { container } from 'tsyringe';
 export default class ProviderAppointmentsController {
   public async index(req: Request, res: Response): Promise<Response> {
     const provider_id = req.user.id;
-    const { day, month, year } = req.body;
+    const { day, month, year } = req.query;
 
     const providerAppointmentsService = container.resolve(
       ListProviderAppointmentsService,
@@ -13,9 +13,9 @@ export default class ProviderAppointmentsController {
 
     const appointments = await providerAppointmentsService.execute({
       provider_id,
-      day,
-      month,
-      year,
+      day: Number(day),
+      month: Number(month),
+      year: Number(year),
     });
 
     return res.status(200).json(appointments);

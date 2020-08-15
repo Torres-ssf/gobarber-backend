40da63cf00c78f8dfc864139abf9e0817c790f9c
Torres <sergio.uft@gmail.com>
2020-08-15 17:07:16 -0300

    refactor: create users/index file to inject user related instances

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,37 +1,21 @@
 import { container } from 'tsyringe';
 
 import './providers';
+import '@modules/users';
+
+// apointments
+// users
+// notification
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import AppointmentsResitory from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 
-import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
-import IUserRepository from '@modules/users/repositories/IUsersRepository';
-
-import IHashProvider from '@modules/users/providers/HashProvider/models/IHashProvider';
-import HashProvider from '@modules/users/providers/HashProvider/implementations/BCryptHashProvider';
-
-import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
-import IUsersTokenRepository from '@modules/users/repositories/IUserTokensRepository';
-
 import INotificationRepository from '@modules/notifications/repositories/INotificationRepository';
 import NotificationRepository from '@modules/notifications/infra/typeorm/repositories/NotificationRepository';
-
-container.registerSingleton<IUserRepository>(
-  'UsersRepository',
-  UsersRepository,
-);
 
 container.registerSingleton<IAppointmentsRepository>(
   'AppointmentsRepository',
   AppointmentsResitory,
-);
-
-container.registerSingleton<IHashProvider>('HashProvider', HashProvider);
-
-container.registerSingleton<IUsersTokenRepository>(
-  'UserTokensRepository',
-  UserTokensRepository,
 );
 
 container.registerSingleton<INotificationRepository>(

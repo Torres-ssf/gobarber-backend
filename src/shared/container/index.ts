b0f0008256e2b0fe4170cbe3068eec41decd59f4
Torres <sergio.uft@gmail.com>
2020-08-15 16:45:06 -0300

    feat: import providers/index file at container/index file

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,7 @@
 import { container } from 'tsyringe';
 
+import './providers';
+
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import AppointmentsResitory from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 

2886377d5ac3a4e895b62eb322808713eb2ae30f
Torres <sergio.uft@gmail.com>
2020-08-15 17:14:13 -0300

    refactor: create notifications/index file

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,17 +1,4 @@
-import { container } from 'tsyringe';
-
 import './providers';
 import '@modules/users';
 import '@modules/appointments';
-
-// apointments
-// users
-// notification
-
-import INotificationRepository from '@modules/notifications/repositories/INotificationRepository';
-import NotificationRepository from '@modules/notifications/infra/typeorm/repositories/NotificationRepository';
-
-container.registerSingleton<INotificationRepository>(
-  'NotificationRepository',
-  NotificationRepository,
-);
+import '@modules/notifications';

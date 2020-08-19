9984ad7098d5b164f245f13cde7f22d2835782a6
Torres-ssf <sergio.uft@gmail.com>
2020-08-19 20:24:33 -0300

    test: add test for retrive data from cache at ListProvidersAppointmentsService

--- src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -12,6 +12,11 @@ let fakeAppointmentsRepository: FakeAppointmentsRepository;
 let listProviderAppointmentsService: ListProviderAppointmentsService;
 let fakeCacheProvider: FakeCacheProvider;
 
+const provider_id = 'provider-id';
+const year = 2020;
+const month = 1;
+const day = 1;
+
 const randomNumber = ({ min, max }: IRandomNumber): number => {
   return min + Math.floor((max - min) * Math.random());
 };
@@ -24,18 +29,13 @@ describe('ListProviderAppointmentsService', () => {
       fakeAppointmentsRepository,
       fakeCacheProvider,
     );
-  });
-
-  it('should be able to list the day availability', async () => {
-    const provider_id = 'provider-id';
-    const year = 2020;
-    const month = 1;
-    const day = 1;
 
     jest.spyOn(Date, 'now').mockImplementation(() => {
       return new Date(year, month, day, 7).getTime();
     });
+  });
 
+  it('should be able to list the day availability', async () => {
     const numOfAppointments = randomNumber({ min: 1, max: 10 });
 
     const appointmentsArr = [];
@@ -57,5 +57,40 @@ describe('ListProviderAppointmentsService', () => {
     );
 
     expect(appointmentsFromProvider.length).toBe(numOfAppointments);
+  });
+
+  it('should be able to get data from the cache after beeing saved', async () => {
+    const save = jest.spyOn(fakeCacheProvider, 'save');
+    const recover = jest.spyOn(fakeCacheProvider, 'recover');
+
+    fakeAppointmentsRepository.create({
+      provider_id: 'provider-id',
+      user_id: 'user01',
+      date: new Date(),
+    });
+
+    await listProviderAppointmentsService.execute({
+      provider_id,
+      day,
+      month: month + 1,
+      year,
+    });
+
+    await listProviderAppointmentsService.execute({
+      provider_id,
+      day,
+      month: month + 1,
+      year,
+    });
+
+    await listProviderAppointmentsService.execute({
+      provider_id,
+      day,
+      month: month + 1,
+      year,
+    });
+
+    expect(save).toHaveBeenCalledTimes(1);
+    expect(recover).toHaveBeenCalledTimes(3);
   });
 });

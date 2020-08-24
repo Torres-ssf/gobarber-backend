f6c9d8f54dfaadeb37cf576fcf7d36e0c2a47fc1
Torres-ssf <sergio.uft@gmail.com>
2020-08-24 15:51:32 -0300

    docs: add User definition to API documentation

--- src/config/documentationUI.ts
+++ src/config/documentationUI.ts
@@ -13,6 +13,40 @@ const apiConfig = {
   schemes: ['http', 'https'],
   consumes: ['application/json'],
   produces: ['application/json'],
+  definitions: {
+    User: {
+      type: 'object',
+      properties: {
+        id: {
+          type: 'string',
+          format: 'uuid',
+        },
+        name: {
+          type: 'string',
+        },
+        email: {
+          type: 'string',
+          format: 'email',
+        },
+        password: {
+          type: 'string',
+          minLength: 6,
+          format: 'password',
+        },
+        avatar: {
+          type: 'string',
+        },
+        created_at: {
+          type: 'string',
+          format: 'date-time',
+        },
+        updated_at: {
+          type: 'string',
+          format: 'date-time',
+        },
+      },
+    },
+  },
 };
 
 export default apiConfig;

71cdd2ed868f049929d0eb3f3f21e6aafe07060a
Torres-ssf <sergio.uft@gmail.com>
2020-08-24 17:17:57 -0300

    docs: documented authentication request

--- src/config/documentationUI.ts
+++ src/config/documentationUI.ts
@@ -28,21 +28,24 @@ const apiConfig = {
           {
             name: 'body',
             in: 'body',
-            description: 'Add new to user in the system',
+            description: 'Add new to user into the system',
             schema: {
               type: 'object',
               properties: {
                 name: {
                   type: 'string',
+                  example: 'Paul Smith',
                 },
                 email: {
                   type: 'string',
                   format: 'email',
+                  example: 'paul_smith@email.com',
                 },
                 password: {
                   type: 'string',
                   format: 'password',
                   minLength: 6,
+                  example: '123456',
                 },
               },
             },
@@ -90,6 +93,99 @@ const apiConfig = {
                 },
                 message: {
                   type: 'string',
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    '/sessions': {
+      post: {
+        tags: ['Sessions'],
+        summary: 'Create a new session into the system.',
+        parameters: [
+          {
+            name: 'body',
+            in: 'body',
+            description:
+              'Create a new session into the system. This request is used for User authentication. When a correct combination of email and password is provided, the request returns all relevant user information for the client-side and a token that grants access to authenticated users',
+            schema: {
+              type: 'object',
+              properties: {
+                email: {
+                  type: 'string',
+                  format: 'email',
+                },
+                password: {
+                  type: 'string',
+                  format: 'password',
+                  minLength: 6,
+                },
+              },
+            },
+          },
+        ],
+        produces: ['application/json'],
+        responses: {
+          '200': {
+            description: 'OK',
+            schema: {
+              type: 'object',
+              properties: {
+                user: {
+                  type: 'object',
+                  properties: {
+                    name: {
+                      type: 'string',
+                      example: 'Paul Smith',
+                    },
+                    email: {
+                      type: 'string',
+                      format: 'email',
+                      example: 'paul_smith@email.com',
+                    },
+                    id: {
+                      type: 'string',
+                      format: 'uuid',
+                    },
+                    created_at: {
+                      type: 'string',
+                      format: 'date-time',
+                    },
+                    updated_at: {
+                      type: 'string',
+                      format: 'date-time',
+                    },
+                    avatar: {
+                      type: 'string',
+                      example: '0745c74169a30e967e59-profile.jpeg',
+                    },
+                    avatar_url: {
+                      type: 'string',
+                    },
+                  },
+                },
+                token: {
+                  type: 'string',
+                  example:
+                    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1OTgyOTczNDUsImV4cCI6MTU5ODM4Mzc0NSwic3ViIjoiZWE4ZWYwM2EtN2FhNi00ZjI5LWEwMjQtZTRhOWFiNzRkYTE3In0.qfEvEXo9Zw9wHWdh5LrAAWlMVWA-d3w1HeHms_ANo70',
+                },
+              },
+            },
+          },
+          '400': {
+            description: 'Bad Request',
+            schema: {
+              type: 'object',
+              properties: {
+                status: {
+                  type: 'string',
+                  example: 'error',
+                },
+                message: {
+                  type: 'string',
+                  example: 'Invalid email/password combination',
                 },
               },
             },

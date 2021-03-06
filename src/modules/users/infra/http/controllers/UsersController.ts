794f9c76c865b2f78368e3702f0f9f9141749cb9
Torres-ssf <sergio.uft@gmail.com>
2021-03-06 13:42:33 -0300

    refactor(user): get provider value from body at UserController

--- src/modules/users/infra/http/controllers/UsersController.ts
+++ src/modules/users/infra/http/controllers/UsersController.ts
@@ -6,11 +6,16 @@ import CreateUserService from '@modules/users/services/CreateUserService';
 
 export default class UsersController {
   public async create(req: Request, res: Response): Promise<Response> {
-    const { name, email, password } = req.body;
+    const { name, email, password, provider } = req.body;
 
     const createUserService = container.resolve(CreateUserService);
 
-    const user = await createUserService.execute({ name, email, password });
+    const user = await createUserService.execute({
+      name,
+      email,
+      password,
+      provider,
+    });
 
     return res.status(200).json(classToClass(user));
   }
